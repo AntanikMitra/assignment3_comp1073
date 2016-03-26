ee2645384567e2f8edb698d91fc8f352933acd62
antanikmitra <antanikmitra@hotmail.com>
2016-03-25 20:19:56 -0400

    added function for parsing ajax with json and added one paragraph

--- Scripts/app.js
+++ Scripts/app.js
@@ -27,5 +27,38 @@
         console.log("Phone: " + phone.value);
         console.log("Comment: " + comment.value);
     });
+    
+    
+     var request = new XMLHttpRequest();
+    request.open('GET', '../info.json', true);
+    request.addEventListener('readystatechange', function() {
+        // wait for file to finish loading
+        if (request.readyState === 4 && request.status === 200) {
+            var information = {};
+
+            // read in the json object
+           information = JSON.parse(request.responseText);
+
+            // declare local paragraph array container
+            var paragraphArray = [];
+            
+            // read in the paragraphs array from the json object
+            paragraphArray = information.paragraphs;
+            
+            // store length of the paragraphArray into a variable
+            var paragraphArrayLength = paragraphArray.length;
+            
+            // loop through the paragraphArray
+            for(var number=0; number < paragraphArrayLength; number++) {
+                // create a reference to each html paragraph element 
+                var paragraph = document.getElementById("paragraph" + (number+1) );
+                
+                // set the innerHTML of the paragraph to the string from the paragraphArray
+                paragraph.innerHTML = paragraphArray[number];
+            }
+
+        }
+    });
+    request.send();
   
   })();
