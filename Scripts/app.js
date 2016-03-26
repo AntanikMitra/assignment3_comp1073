3655339cfd5a3d830fbacee549ca8d552d407fad
antanikmitra <antanikmitra@hotmail.com>
2016-03-25 22:36:18 -0400

    updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -11,6 +11,27 @@
 
 
     "use strict";
+    
+    // initializing variable in contact page form
+    var firstName = document.getElementById("firstName");
+    var lastName = document.getElementById("lastName");
+    var email = document.getElementById("email");
+    var phone = document.getElementById("phone");
+    var comment = document.getElementById("comment");
+    var submitButton = document.getElementById("submitButton");
+    submitButton.pre
+
+ // adding the event listener
+    submitButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        console.log("First Name: " + firstName.value);
+        console.log("Last Name: " + lastName.value);
+        console.log("Email: " + email.value);
+        console.log("Phone: " + phone.value);
+        console.log("Comment: " + comment.value);
+    });
+    
+    
     var request = new XMLHttpRequest();
     request.open('GET', '../projects.json', true);
     request.addEventListener('readystatechange', function () {
