29dad1c672f78fba2cfc8ed44f758a5ba003f0c2
antanikmitra <antanikmitra@hotmail.com>
2016-03-25 22:13:03 -0400

    Updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -5,60 +5,41 @@
   Website: http://tom-assignment3.azurewebsites.net/
 */
 
-"use strict";
+
 // setup your IIFE(Immediately Invoked Function Expression)
 (function () {
-/*
-   // initializing variable in contact page form
-    var firstName = document.getElementById("firstName");
-    var lastName = document.getElementById("lastName");
-    var email = document.getElementById("email");
-    var phone = document.getElementById("phone");
-    var comment = document.getElementById("comment");
-    var submitButton = document.getElementById("submitButton");
-    submitButton.pre
 
- // adding the event listener
-    submitButton.addEventListener("click", function (event) {
-        event.preventDefault();
-        console.log("First Name: " + firstName.value);
-        console.log("Last Name: " + lastName.value);
-        console.log("Email: " + email.value);
-        console.log("Phone: " + phone.value);
-        console.log("Comment: " + comment.value);
-    });
-    
- */   
-     var request = new XMLHttpRequest();
-    request.open('GET', '../info.json', true);
-    request.addEventListener('readystatechange', function() {
-        // wait for file to finish loading
-        if (request.readyState === 4 && request.status === 200) {
-            var information = {};
 
-            // read in the json object
-           information = JSON.parse(request.responseText);
-
-            // declare local paragraph array container
+    "use strict";
+    var request = new XMLHttpRequest();
+    request.open('GET', '../projects.json', true);
+    request.addEventListener('readystatechange', function () {
+        if (request.readyState === 4) {
+            // creating a variable for object
+            var projects = {};
+        
+            // parse the json object
+            projects = JSON.parse(request.responseText);
+         
+            // declare local paragraph array 
             var paragraphArray = [];
             
-            // read in the paragraphs array from the json object
-            paragraphArray = information.paragraphs;
+            // read in paragraph array
+            paragraphArray = projects.paragraphs;
             
-            // store length of the paragraphArray into a variable
+            //storing length of array
             var paragraphArrayLength = paragraphArray.length;
             
-            // loop through the paragraphArray
-            for(var number=0; number < paragraphArrayLength; number++) {
-                // create a reference to each html paragraph element 
-                var paragraph = document.getElementById("paragraph" + (number+1) );
+            // looping through array
+            for(var number=0; number < paragraphArrayLength; number++){
+              
+              // creating a reference
+              var paragraph = document.getElementById("paragraph" + (number+1) );
+              paragraph.innerHTML = paragraphArray[number];  
                 
-                // set the innerHTML of the paragraph to the string from the paragraphArray
-                paragraph.innerHTML = paragraphArray[number];
             }
-
         }
     });
     request.send();
-  
-  })();
+
+})();
